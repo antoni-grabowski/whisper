509ea94f875aae0f0826fd0060fe228bde603681
Antoni Grabowski <grabowski.antoni@proton.me>
2026-05-25 11:41:35 +0200

    Initial next.js project

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader } from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <main>
+      <Card>
+        <CardHeader></CardHeader>
+      </Card>
+    </main>
+  );
+}
